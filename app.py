e57208eefc5bbe5523770e76e6c8141448112033
Jessica <jeha6343@colorado.edu>
2024-04-03 02:07:33 +0000

    added db_create

--- app.py
+++ app.py
@@ -13,3 +13,25 @@ def db_test():
     conn = psycopg2.connect("postgres://jc_database_user:P0NCJOjJNYstCiEBy2O3Ox8uSPf6vDCg@dpg-co6b5eq0si5c73cd2kng-a/jc_database")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def creating():
+        conn = psycopg2.connect("postgres://jc_database_user:P0NCJOjJNYstCiEBy2O3Ox8uSPf6vDCg@dpg-co6b5eq0si5c73cd2kng-a/jc_database")
+        cur = conn.cursor()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+            ''')
+        conn.commit()
+        conn.close()
+        return "Basketball Table Successfully Created"
+        
+        
+        
+        
+        
